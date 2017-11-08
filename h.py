312392d24de5ca3954e76510ef110bbd1c3f7ae2
Shobha Pola -X (shopola - XEROX CORP at Cisco) <shopola@cisco.com>
2017-11-08 22:08:37 +0530

    second

---
+++ h.py
@@ -0,0 +1,22 @@
+str1="zhobha,i,A,a garl"
+n=str1.replace('a','w')
+m=str1.rstrip()
+print(n)
+print(m)
+z=str1.split(",")
+print(z)
+f=str1.swapcase()
+print(f)
+l1=[1,2,45,67,89]
+l2=[98,78,90,2,1]
+g=len(l1)
+print(g)
+j=max(l1)
+l1.append(100)
+n=l1.extend(l2)
+print(l1)
+
+h=min(l2)
+print(h)
+l=tuple(l1)
+print(l)
